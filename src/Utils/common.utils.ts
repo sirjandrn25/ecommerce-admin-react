b1ebff9376fbf8a6bf6432fa484d4d6294f0f455
Sirjan Tamang <sirjantmg99@gmail.com>
2023-02-14 09:36:31 +0545

    made debounce

--- src/Utils/common.utils.ts
+++ src/Utils/common.utils.ts
@@ -5,3 +5,14 @@ export const EmptyFunction = () => {
 export const resolveNavigation = (path: string) => {
 	return `/admin/${path}`;
 };
+
+export const Debounce = (func: () => void, wait: number) => {
+	let interval: any;
+	return (...args: any) => {
+		const context = this;
+		clearTimeout(interval);
+		interval = setTimeout(() => {
+			func.apply(context, args);
+		}, wait);
+	};
+};
